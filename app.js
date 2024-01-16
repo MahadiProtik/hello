9ff424c6309fb9cc1ea23808082bfc1378b33fed
Mahadi Hashan Protik <105696757+MahadiProtik@users.noreply.github.com>
2024-01-16 21:09:20 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+console.log("Hello");
+
+// `Hello JAVASCRIPT`
+
+let Fruit = "Orange";
+
+console.log(`Hello ${Fruit}`);
+
+let x = 25;
+let y = 65;
+
+console.log(`${x}+${y} = ${x+y}`);
+
+
+//Arithmetic Operators
+
+
+
+//Logical Operators
+
+
+//Assignment Operators
+
+
+//Comparison Operators
+
+
